672941ef9bc1e8b0ad55e59ad22d9de3b4f22e02
unknown <delfdevs@gmail.com>
2019-12-11 15:36:30 -0500

    msgquickchange

--- client/src/App.js
+++ client/src/App.js
@@ -17,9 +17,8 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
       <Route exact path='/' component={MovieList}/>
-      <Route path='/Movies/:id' component={Movie}/>
+      <Route path='/movies/:id' component={Movie}/>
     </div>
   );
 };
